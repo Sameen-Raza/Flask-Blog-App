1365fe4369069778486a120d5f0876093763b802
Sameen-Raza <sameenraza987@gmail.com>
2025-05-23 13:02:51 +0530

    Initial commit with Flask app and database setup

--- migrations/versions/3c98b7527df9_initial_migration.py
+++ migrations/versions/3c98b7527df9_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: eed9a8be7d89
+Revision ID: 3c98b7527df9
 Revises: 
-Create Date: 2025-05-23 12:49:38.896219
+Create Date: 2025-05-23 13:02:21.598006
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'eed9a8be7d89'
+revision = '3c98b7527df9'
 down_revision = None
 branch_labels = None
 depends_on = None
